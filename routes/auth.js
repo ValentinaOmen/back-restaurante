fae66cd40de135501f74dd4e705ff371095aa588
ValentinaOmen <moralesomenvalentina@gmail.com>
2025-01-29 19:52:41 -0500

    este es nuebo cambio borando  el env  que estan porfuera de src

--- routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login } = require("../controllers/authController");
-
-// Ruta para autenticar al usuario
-router.post("/login", login);
-
-module.exports = router;
